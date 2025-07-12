d3724112536c99313a75c461bca38c3f0a197996
hitaksha <60245659+hitaksha@users.noreply.github.com>
2025-07-12 07:56:44 +0530

    Added CreditMax logo to WhatsApp widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import logo from './assets/logo.jpeg';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -35,19 +36,21 @@ function App() {
 
       {/* 🟢 Floating WhatsApp Widget */}
 	<FloatingWhatsApp
-  	  phoneNumber="919987593277"
-  	  accountName="CreditMax"
-  	  chatMessage="Hello! How can we help you today?"
-  	  statusMessage="Typically replies within minutes"
-  	  allowClickAway
-  	  notification
-  	  onClick={() => {
-    	    window.gtag?.('event', 'whatsapp_click', {
-      	       event_category: 'engagement',
-      	       event_label: 'Floating Widget',
-    	    });
-  	  }}
-	/>
+  	phoneNumber="919987593277"
+ 	accountName="CreditMax"
+  	chatMessage="Hello! How can we help you today?"
+  	statusMessage="Typically replies within minutes"
+  	avatar={logo} // ✅ Uses your JPEG logo
+  	allowClickAway
+  	notification
+  	onClick={() => {
+   	  window.gtag?.('event', 'whatsapp_click', {
+      	  event_category: 'engagement',
+      	  event_label: 'Floating Widget',
+        });
+       }}
+     />
+
 
 
       <Footer />
