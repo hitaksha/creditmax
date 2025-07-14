645effb7494653710f4c367e84de4aab3245906f
hitaksha <60245659+hitaksha@users.noreply.github.com>
2025-07-14 21:38:47 +0530

    Improved SEO: Added canonical tag

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2019',
+    rollupOptions: {
+      external: ['react-helmet'],
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
